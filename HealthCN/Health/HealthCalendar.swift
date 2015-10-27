59d01f4684a8514e270f0836d2262fcdb45bdb76
kevintang1020 <kevintang1020@gmail.com>
2015-10-28 00:53:56 +0800

    2015/10/28 home

--- HealthCN/Health/HealthCalendar.swift
+++ HealthCN/Health/HealthCalendar.swift
@@ -17,6 +17,12 @@ class HealthCalendar: UIViewController {
     private var pubClass: PubClass!
     private let mAppDelegate = UIApplication.sharedApplication().delegate! as! AppDelegate
     
+    // calendar 相關
+    private var today: String = ""
+    
+    //let mCalendar = NSCalendar.currentCalendar()
+    let mCalendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+
     // 本 class 需要使用的 json data
     var dictAllData: [[String: AnyObject]] = []
     
@@ -36,8 +42,88 @@ class HealthCalendar: UIViewController {
             // HTTP 連線取得本頁面需要的資料
             //self.StartHTTPConn()
             
+            // Calendar init
+            self.initCalendarParm()
+            
             return
         }
+    }
+    
+    /**
+    * 回傳格式化後的 日期/時間
+    * http://www.codingexplorer.com/swiftly-getting-human-readable-date-nsdateformatter/
+    */
+    func getFormatYMD(mDate: NSDate)->String {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy"
+        
+        return dateFormatter.stringFromDate(mDate)
+    }
+    
+    /**
+    * 初始 NSCalendar, NSDate 相關參數<BR>
+    */
+    func initCalendarParm() {
+        let mNSDate = NSDate()
+        let components = mCalendar!.components(NSCalendarUnit.Month, fromDate: mNSDate)
+        components.year = 2015
+        components.month = 10
+        
+        // Getting the First and Last date of the month
+        components.day = 1
+        let firstDateOfMonth: NSDate = mCalendar!.dateFromComponents(components)!
+        print(getFormatYMD(firstDateOfMonth))
+        
+        components.month  += 1
+        components.day     = 0
+        let lastDateOfMonth: NSDate = mCalendar!.dateFromComponents(components)!
+        print(getFormatYMD(lastDateOfMonth))
+        
+        
+        
+        /*
+
+        // Getting the First and Last date of the month
+        components.day = 1
+        let firstDateOfMonth: NSDate = calendar.dateFromComponents(components)!
+        
+        components.month  += 1
+        components.day     = 0
+        let lastDateOfMonth: NSDate = calendar.dateFromComponents(components)!
+        
+        var unitFlags = NSCalendarUnit.WeekOfMonthCalendarUnit |
+        NSCalendarUnit.WeekdayCalendarUnit     |
+        NSCalendarUnit.CalendarUnitDay
+        
+        let firstDateComponents = calendar.components(unitFlags, fromDate: firstDateOfMonth)
+        let lastDateComponents  = calendar.components(unitFlags, fromDate: lastDateOfMonth)
+        
+        // Sun = 1, Sat = 7
+        let firstWeek = firstDateComponents.weekOfMonth
+        let lastWeek  = lastDateComponents.weekOfMonth
+        
+        let numOfDatesToPrepend = firstDateComponents.weekday - 1
+        let numOfDatesToAppend  = 7 - lastDateComponents.weekday + (6 - lastDateComponents.weekOfMonth) * 7
+        
+        let startDate: NSDate = calendar.dateByAddingUnit(NSCalendarUnit.CalendarUnitDay, value: -numOfDatesToPrepend, toDate: firstDateOfMonth, options: nil)!
+        let endDate:   NSDate = calendar.dateByAddingUnit(NSCalendarUnit.CalendarUnitDay, value: numOfDatesToAppend, toDate: lastDateOfMonth, options: nil)!
+        
+        Array(map(0..<42) {
+        calendar.dateByAddingUnit(NSCalendarUnit.CalendarUnitDay, value: $0, toDate: startDate, options: nil)!
+        })
+        
+        "\(components.year)"
+        
+        
+        var dateString = "2014-10-3" // change to your date format
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "YYYY-MM-dd"
+        
+        var someDate = dateFormatter.dateFromString(dateString)
+        println(someDate)
+
+        */
+        
     }
     
     /**
