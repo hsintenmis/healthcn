e65e3e02ead15895cdc8a1610dceda3f28c9b89b
hsintenmis <htemis@htemisdeimac.local>
2016-03-02 08:44:39 +0800

    2016/03/02 office

--- HealthCN/BTDevice/BTDeviceList.swift
+++ HealthCN/BTDevice/BTDeviceList.swift
@@ -32,12 +32,14 @@ class BTDeviceList: UITableViewController {
         }
         
         // 藍芽血壓計
+        /*
         if segue.identifier == "BTBPMain"{
             let cvChild = segue.destinationViewController as! BTBPMain
             cvChild.dictMember = dictMember
             
             return
         }
+        */
 
         return
     }
